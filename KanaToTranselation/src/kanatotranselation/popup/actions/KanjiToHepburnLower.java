88e478529fca10f24b4683904a8eed146e46c784
syany <kz.github.syany@gmail.com>
2014-10-01 00:38:25 +0900

    更新

---
+++ KanaToTranselation/src/kanatotranselation/popup/actions/KanjiToHepburnLower.java
@@ -0,0 +1,48 @@
+/**
+ *
+ */
+package kanatotranselation.popup.actions;
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IEditorActionDelegate;
+import org.eclipse.ui.IEditorPart;
+import org.uranoplums.typical.lang.UraObject;
+
+/**
+ * @author syany
+ *
+ */
+public class KanjiToHepburnLower extends UraObject implements IEditorActionDelegate {
+
+	/** ダイアログ */
+	private Shell shell;
+	/** 対象カレントエディタ */
+	private IEditorPart targetEditor;
+
+	/*
+	 * @see IActionDelegate#run(IAction)
+	 */
+	@Override
+	public void run(IAction action) {
+		Translation.getInstance().kanjiToHepburnLower(targetEditor);  //getKanjiToHepburn
+
+		MessageDialog.openInformation(
+			shell,
+			"KanaToTranselation",
+			"Kana to Hepburn was executed.");
+	}
+
+	@Override
+	public void setActiveEditor(IAction action, IEditorPart targetEditor) {
+		this.targetEditor = targetEditor;
+		shell = targetEditor.getSite().getShell();
+	}
+
+	@Override
+	public void selectionChanged(IAction action, ISelection selection) {
+	}
+
+}
